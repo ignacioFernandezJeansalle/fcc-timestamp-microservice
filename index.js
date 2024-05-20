60c9595b666b3193f8a3cdd618df6e5ddcd83e6d
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-05-20 10:53:54 -0300

    Refactorizacion

--- index.js
+++ index.js
@@ -26,41 +26,38 @@ app.get("/", function (req, res) {
 */
 
 app.get("/api/:date?", function (req, res) {
-  // An empty date parameter should return the current time in a JSON object with a unix key and utc key
-  if (!req.params.date) {
+  const paramsDate = req.params.date;
+
+  // Parámetro date vacío
+  if (!paramsDate) {
     const unix = Date.now();
     const utc = new Date(unix).toUTCString();
 
     return res.json({ unix: unix, utc: utc });
   }
 
-  const paramsDate = req.params.date;
-
-  // A request to /api/:date? with a valid date should return a JSON object with a unix key that is a Unix timestamp of the input date in milliseconds (as type Number).
-  // A request to /api/:date? with a valid date should return a JSON object with a utc key that is a string of the input date in the format: Thu, 01 Jan 1970 00:00:00 GMT
+  // Parámetro date = date
   const date = Date.parse(paramsDate);
-  const isDate = !isNaN(date);
+  const isValidDate = !isNaN(date);
 
-  if (isDate) {
+  if (isValidDate) {
     const unix = date;
     const utc = new Date(unix).toUTCString();
 
     return res.json({ unix: unix, utc: utc });
   }
 
-  // A request to /api/1451001600000 should return { unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" }
-  const isUnix = new Date(parseInt(paramsDate)).toString() !== "Invalid Date";
+  // Parámetro date = unix
+  const unix = parseInt(paramsDate);
+  const isValidUnix = new Date(unix).toString() !== "Invalid Date";
 
-  console.log({ paramsDate, isDate, isUnix });
-
-  if (isUnix) {
-    const unix = parseInt(paramsDate);
+  if (isValidUnix) {
     const utc = new Date(unix).toUTCString();
 
     return res.json({ unix: unix, utc: utc });
   }
 
-  // If the input date string is invalid, the API returns an object having the structure { error : "Invalid Date" }
+  // Parámetro date = inválido
   return res.json({ error: "Invalid Date" });
 });
 
