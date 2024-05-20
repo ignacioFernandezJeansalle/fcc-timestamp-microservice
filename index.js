938d08ea13731e00f2887723d6439111e3b6ed8d
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-05-20 10:42:37 -0300

    Agrego validaciones para unix y date

--- index.js
+++ index.js
@@ -19,9 +19,8 @@ app.get("/", function (req, res) {
 });
 
 /* 
-[ ] A request to /api/:date? with a valid date should return a JSON object with a unix key that is a Unix timestamp of the input date in milliseconds (as type Number).
-[ ] A request to /api/:date? with a valid date should return a JSON object with a utc key that is a string of the input date in the format: Thu, 01 Jan 1970 00:00:00 GMT
-[ ] A request to /api/1451001600000 should return { unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" }
+
+
 [ ] 
 
 */
@@ -35,17 +34,34 @@ app.get("/api/:date?", function (req, res) {
     return res.json({ unix: unix, utc: utc });
   }
 
-  const date = new Date(req.params.date).toString();
+  const paramsDate = req.params.date;
 
-  // If the input date string is invalid, the API returns an object having the structure { error : "Invalid Date" }
-  if (date === "Invalid Date") {
-    return res.json({ error: "Invalid Date" });
+  // A request to /api/:date? with a valid date should return a JSON object with a unix key that is a Unix timestamp of the input date in milliseconds (as type Number).
+  // A request to /api/:date? with a valid date should return a JSON object with a utc key that is a string of the input date in the format: Thu, 01 Jan 1970 00:00:00 GMT
+  const date = Date.parse(paramsDate);
+  const isDate = !isNaN(date);
+
+  if (isDate) {
+    const unix = date;
+    const utc = new Date(unix).toUTCString();
+
+    return res.json({ unix: unix, utc: utc });
   }
 
-  res.json({
-    unix: 1451001600000,
-    utc: "Fri, 25 Dec 2015 00:00:00 GMT",
-  });
+  // A request to /api/1451001600000 should return { unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" }
+  const isUnix = new Date(parseInt(paramsDate)).toString() !== "Invalid Date";
+
+  console.log({ paramsDate, isDate, isUnix });
+
+  if (isUnix) {
+    const unix = parseInt(paramsDate);
+    const utc = new Date(unix).toUTCString();
+
+    return res.json({ unix: unix, utc: utc });
+  }
+
+  // If the input date string is invalid, the API returns an object having the structure { error : "Invalid Date" }
+  return res.json({ error: "Invalid Date" });
 });
 
 // Listen on port set in environment variable or default to 3000
